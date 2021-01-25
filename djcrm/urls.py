201f9222ede82882031e59c1b4530a5207abc01f
Mishra-raja <rajamishra0702@gmail.com>
2021-01-25 22:58:18 +0530

    initial commit for the CRM app

---
+++ djcrm/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from leads.views import landing_page
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('leads/', include('leads.urls', namespace='leads')),
+    path('',landing_page,name='landing-page')
+    
+]
